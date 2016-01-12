f95846293a57f747302fa31c050b4617aa8291de
Saúl Cabrera <saulecabrera@gmail.com>
2016-01-12 08:13:50 -0600

    file structure and controllers

--- web/views/v1/todo_view.ex
+++ web/views/v1/todo_view.ex
@@ -1,12 +1,12 @@
-defmodule TodoxApi.TodoView do
+defmodule TodoxApi.V1.TodoView do
   use TodoxApi.Web, :view
 
   def render("index.json", %{todos: todos}) do
-    %{data: render_many(todos, TodoxApi.TodoView, "todo.json")}
+    %{data: render_many(todos, TodoxApi.V1.TodoView, "todo.json")}
   end
 
   def render("show.json", %{todo: todo}) do
-    %{data: render_one(todo, TodoxApi.TodoView, "todo.json")}
+    %{data: render_one(todo, TodoxApi.V1.TodoView, "todo.json")}
   end
 
   def render("todo.json", %{todo: todo}) do
