358f0d500fb801a6d51bc276435a44456943239d
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-06 17:19:58 -0400

    updated dependencies

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Todox.Mixfile do
   def project do
     [app: :todox,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -30,7 +30,7 @@ defmodule Todox.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.2"},
+    [{:phoenix, "~> 1.2.0"},
      {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.3"},
