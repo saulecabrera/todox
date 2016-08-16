71736f6078e17468901f951de9b911a7fd311852
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-16 18:55:47 -0400

    added tests for user model

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -8,16 +8,39 @@ defmodule Todox.UserTest do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
   end
 
-  @valid_attrs %{password: "some content", password_hash: "some content", username: "some content"}
+  @valid_attrs %{password: "some content", username: "user_name"}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = User.changeset(%User{}, @valid_attrs)
+  test "user registration with valid username and password" do
+    changeset = User.registration_changeset(%User{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = User.changeset(%User{}, @invalid_attrs)
+  test "changeset with empty attributes" do
+    changeset = User.registration_changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "changeset with invalid username on registration (containing spaces)" do
+    changeset = User.registration_changeset(%User{}, %{username: "username with spaces"})
+    refute changeset.valid?
+  end
+
+  test "changeset with invalid username on registration (containing special characters other than _)" do
+    changeset = User.registration_changeset(%User{}, %{username: "no_spaces_other_char%?"})
+    refute changeset.valid?
+  end
+
+  test "changeset with invalid password on registration (invalid length)" do
+    changeset = User.registration_changeset(%User{}, %{password: "1234567"})
+    refute changeset.valid?
+  end
+
+  test "fails when trying to insert duplicate usernames" do
+    changeset = User.registration_changeset(%User{}, %{password: "12345678", username: "00xxFF"})
+    {:ok, _} = Repo.insert(changeset)
+
+    changeset_with_constraint = User.registration_changeset(%User{}, %{password: "some password here", username: "00xxFF"})
+    refute match? {:error, ^changeset_with_constraint}, Repo.insert(changeset_with_constraint)
   end
 end
