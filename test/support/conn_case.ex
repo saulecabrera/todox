0762fac06f332749ecfba3feb7d8c20408e2dddc
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-18 20:56:47 -0400

    get rid of build_conn warning

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -37,6 +37,6 @@ defmodule Todox.ConnCase do
       Ecto.Adapters.SQL.restart_test_transaction(Todox.Repo, [])
     end
 
-    {:ok, conn: Phoenix.ConnTest.conn()}
+    {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
