709973bc99be14e1b5ac29b74c50e5baa058f4aa
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-21 20:04:47 -0400

    added login in registration

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -8,9 +8,16 @@ defmodule Todox.UserController do
 
     case Repo.insert(changeset) do
       {:ok, user} ->
-        conn
+        new_conn = Guardian.Plug.api_sign_in(conn, user)
+        jwt = Guardian.Plug.current_token(new_conn)
+        {:ok, claims} = Guardian.Plug.claims(new_conn)
+        exp = Map.get(claims, "exp")
+         
+        new_conn
         |> put_status(:created)
-        |> render("show.json", user: user)
+        |> put_resp_header("authorization", "Bearer #{jwt}")
+        |> put_resp_header("x-expires", to_string(exp))
+        |> render("registration.json", user: user, jwt: jwt, exp: exp)
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
