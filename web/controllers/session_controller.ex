bbe4f754c4329b9911976d7dc34a2d3d1ceafd02
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-24 19:16:58 -0400

    added create session

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,9 +1,25 @@
-defmodule SessionController do
+defmodule Todox.SessionController do
   use Todox.Web, :controller
 
   alias Todox.User
+  alias Todox.Auth
 
-  def create(conn, %{"credentials" => credentials}) do
-    
+  def create(conn, %{"credentials" => %{"username" => username, "password" => pw}}) do
+    case Auth.login_by_username(username, pw) do
+      {:ok, user} ->
+        {:ok, conn, jwt, claims, exp} = Auth.generate_jwt(conn, user)
+
+        conn
+        |> put_status(:created)
+        |> put_resp_header("authorization", "Bearer #{jwt}")
+        |> put_resp_header("x-expires", to_string(exp))
+        |> render("registration.json", user: user, jwt: jwt, exp: exp)
+      {:error, :unathorized} ->
+        conn
+        |> put_status(:unprocessable_entity)
+      {:error, :not_found} ->
+        conn
+        |> put_status(:not_found)
+    end
   end
 end
