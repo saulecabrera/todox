73fa9ef78c763563c38122dc807f2779684d7c8e
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-24 16:44:27 -0400

    added session test

---
+++ test/controllers/session_controller_test.exs
@@ -0,0 +1,19 @@
+defmodule Todox.SessionControllerTest do
+  alias Todox.Repo
+  alias Todox.User
+
+  use Todox.ConnCase
+
+  @invalid_login %{username: "nonexistent", password: "wrong_password"}
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  test "login: error 401 and errors are returned when bad credentials or non existing user is provided",
+  %{conn: conn} do
+    conn = post conn, login_path(conn, :create), credentials: @invalid_login
+    body = json_response(conn, 401)
+    assert body["errors"]
+  end
+end
