3633a3d656847b24d280cd2cf9c46b7fd28992b1
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-24 18:35:24 -0400

    refactred out functionality for jwt

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,18 +2,16 @@ defmodule Todox.UserController do
   use Todox.Web, :controller
 
   alias Todox.User
-
+  alias Todox.Auth
+  
   def create(conn, %{"user" => user_params}) do
     changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
-        new_conn = Guardian.Plug.api_sign_in(conn, user)
-        jwt = Guardian.Plug.current_token(new_conn)
-        {:ok, claims} = Guardian.Plug.claims(new_conn)
-        exp = Map.get(claims, "exp")
-         
-        new_conn
+        {conn, jwt, claims, exp} = Auth.generate_jwt(conn, user)
+
+        conn
         |> put_status(:created)
         |> put_resp_header("authorization", "Bearer #{jwt}")
         |> put_resp_header("x-expires", to_string(exp))
