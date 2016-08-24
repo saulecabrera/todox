178fc1112cbca4b797aa39f640ec04547c1671b4
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-24 18:34:48 -0400

    added auth module

---
+++ web/auth/auth.ex
@@ -0,0 +1,11 @@
+defmodule Todox.Auth do
+
+  def generate_jwt(conn, user) do
+    new_conn = Guardian.Plug.api_sign_in(conn, user)
+    jwt = Guardian.Plug.current_token(new_conn)
+    {:ok, claims} = Guardian.Plug.claims(new_conn)
+    exp = Map.get(claims, "exp")
+
+    {new_conn, jwt, claims, exp}
+  end
+end
