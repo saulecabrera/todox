24ea90b5b993fd47f868d34b7c3c153361da0117
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-24 18:48:52 -0400

    added function to lookup user by username

--- web/auth/auth.ex
+++ web/auth/auth.ex
@@ -1,11 +1,27 @@
 defmodule Todox.Auth do
 
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+  alias Todox.Repo
+  alias Todox.User
+
   def generate_jwt(conn, user) do
     new_conn = Guardian.Plug.api_sign_in(conn, user)
     jwt = Guardian.Plug.current_token(new_conn)
     {:ok, claims} = Guardian.Plug.claims(new_conn)
     exp = Map.get(claims, "exp")
 
-    {new_conn, jwt, claims, exp}
+    {:ok, new_conn, jwt, claims, exp}
+  end
+
+  def login_by_username(conn, username, given_pw) do
+    user = Repo.get_by(User, username: username)
+    cond do
+      user && checkpw(given_pw, user.password_hash) ->
+        {:ok, conn, user}
+      user ->
+        {:error, :unauthorized, conn}
+      true ->
+        {:error, :not_found, conn}
+    end
   end
 end
