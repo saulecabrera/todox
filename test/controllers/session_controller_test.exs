9f9e3a046fbcb12b0899a2d811df032688f3155e
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-26 16:48:13 -0400

    more tests on session

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -5,7 +5,6 @@ defmodule Todox.SessionControllerTest do
   use Todox.ConnCase
 
   @invalid_login %{username: "nonexistent", password: "wrong_password"}
-  @existing_user_wrong_pw %{username: "username", password: "wrong_password"}
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -23,7 +22,21 @@ defmodule Todox.SessionControllerTest do
     changeset = User.registration_changeset(%User{}, user_params)
     {:ok, user} = Repo.insert(changeset)
 
-    conn = post conn, login_path(conn, :create), credentials: @existing_user_wrong_pw
+    existing_user_wrong_pw =  %{username: "username", password: "wrong_password"}
+    conn = post conn, login_path(conn, :create), credentials: existing_user_wrong_pw
     assert 401 == conn.status
+  end
+
+  test "login: status 200 and token response when valid credentials supplied", 
+  %{conn: conn} do
+    user_params = %{username: "username", password: "secretpassword"}
+    changeset = User.registration_changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+
+    existing_user_correct_pw = %{username: "username", password: "secretpassword"}
+    conn = post conn, login_path(conn, :create), credentials: existing_user_correct_pw
+    body = json_response(conn, 200)
+
+    assert body["data"]
   end
 end
