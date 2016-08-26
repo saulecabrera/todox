41edd33677a83d61a8573db95dab5345c958948d
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-25 20:08:34 -0400

    added unauthorized clause

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -14,9 +14,9 @@ defmodule Todox.SessionController do
         |> put_resp_header("authorization", "Bearer #{jwt}")
         |> put_resp_header("x-expires", to_string(exp))
         |> render("registration.json", user: user, jwt: jwt, exp: exp)
-      {:error, :unathorized} ->
+      {:error, :unauthorized} ->
         conn
-        |> put_status(:unprocessable_entity)
+        |> put_status(:unauthorized)
       {:error, :not_found} ->
         conn
         |> put_status(:not_found)
