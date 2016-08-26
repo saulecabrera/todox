cb6e534bd016892f49a30bd63f98dc6145c02465
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-25 20:08:57 -0400

    more test on session

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -5,14 +5,25 @@ defmodule Todox.SessionControllerTest do
   use Todox.ConnCase
 
   @invalid_login %{username: "nonexistent", password: "wrong_password"}
+  @existing_user_wrong_pw %{username: "username", password: "wrong_password"}
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  test "login: error 401 and errors are returned when bad credentials or non existing user is provided",
+  test "login: status 401 and returned when non existing user is provided",
   %{conn: conn} do
     conn = post conn, login_path(conn, :create), credentials: @invalid_login
     assert 404 == conn.status
+  end
+
+  test "login: status 401 when existing user and bad password provided", 
+  %{conn: conn} do
+    user_params = %{username: "username", password: "secretpassword"}
+    changeset = User.registration_changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+
+    conn = post conn, login_path(conn, :create), credentials: @existing_user_wrong_pw
+    assert 401 == conn.status
   end
 end
