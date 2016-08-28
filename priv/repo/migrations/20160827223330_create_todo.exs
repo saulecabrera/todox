759b21e29b08aa24fcd4eec422990f6c3e7628bc
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-28 10:07:55 -0400

    added todos migrations

---
+++ priv/repo/migrations/20160827223330_create_todo.exs
@@ -0,0 +1,16 @@
+defmodule Todox.Repo.Migrations.CreateTodo do
+  use Ecto.Migration
+
+  def change do
+    create table(:todos) do
+      add :title, :string
+      add :description, :text
+      add :completed, :boolean, default: false, null: false
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+    create index(:todos, [:user_id])
+
+  end
+end
