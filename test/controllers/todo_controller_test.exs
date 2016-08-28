9e54bb747029cb6f7634ef843cbbb9d3e7dea38f
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-28 16:38:24 -0400

    added first test for todo controller

--- test/controllers/todo_controller_test.exs
+++ test/controllers/todo_controller_test.exs
@@ -1,15 +1,24 @@
 defmodule Todox.TodoControllerTest do
   use Todox.ConnCase
 
-  alias Todox.Todo
+  alias Todox.{Repo, User, Todo, Auth}
+
+  @user %User{username: "johndoe", password: "strongpassword"}
   @valid_attrs %{completed: true, description: "some content", title: "some content"}
   @invalid_attrs %{}
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    {:ok, user} = Repo.insert(@user) 
+    {:ok, conn, jwt, claims, _} = Auth.generate_jwt(conn, user)
+    conn = put_req_header(conn, "accept", "application/json")
+    {:ok, conn: conn, jwt: jwt, user: user, claims: claims}
   end
 
-  test "POST /todos creates a todo for a authenticated user", %{conn: conn} do
-    true
+  test "POST /todos creates a todo when request contains a valid jwt", 
+  %{conn: conn, jwt: jwt} do
+    conn = conn |> put_req_header("authorization", "Bearer #{jwt}")  
+    conn = post conn, todo_path(conn, :create), todo: @valid_attrs 
+    
+    body = json_response(conn, 201)
   end
 end
