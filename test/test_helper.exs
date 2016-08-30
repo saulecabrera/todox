4b2b6fa45939a1d24cd3a3edbfaace7b48081937
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-30 16:38:42 -0400

    added ex_machina

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,4 +3,5 @@ ExUnit.start
 Mix.Task.run "ecto.create", ~w(-r Todox.Repo --quiet)
 Mix.Task.run "ecto.migrate", ~w(-r Todox.Repo --quiet)
 Ecto.Adapters.SQL.Sandbox.mode(Todox.Repo, :manual)
+{:ok, _} = Application.ensure_all_started(:ex_machina)
 
