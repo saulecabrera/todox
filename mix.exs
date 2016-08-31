37e7f46edc6542e78bd18b605f40fb2ab3db7a42
Saúl Cabrera <saulecabrera@gmail.com>
2016-08-31 18:54:52 -0400

    removed exmachina from apps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Todox.Mixfile do
   def application do
     [mod: {Todox, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :comeonin, :ex_machina]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
